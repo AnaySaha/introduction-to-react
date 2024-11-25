cab8b921e1b0abd595bb35cc6db8eb6fcb72250f
Anay Saha <anaysahapaban@gmail.com>
2024-11-26 03:07:25 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../assets/Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+
+            <Header></Header>
+            <h2>This is the home componets</h2>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,32 @@ import {
   RouterProvider,
 } 
 from "react-router-dom";
+import About from './components/Home/About/About';
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello world!</div>,
+    path: '/',
+    element: <Home>This is the home</Home>
+    <Outlet></Outlet>
   },
 
   {
     path: '/about',
     element: <div> I am in the about page</div>
-  }
+  },
+
+  {
+    path: '/',
+    element: <Home></Home>
+    children: [
+      {
+        path: '/about'
+        element: <About></About>
+      }
+    ]
+  },
+
 ]);
 
 createRoot(document.getElementById('root')).render(
