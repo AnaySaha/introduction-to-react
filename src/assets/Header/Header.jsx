547eefe4539d0d53cd40bc125b582b77d890f384
Anay Saha <anaysahapaban@gmail.com>
2024-12-02 02:23:35 +0600

    add

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -1,10 +1,14 @@
-
+import { Link, NavLink} from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
-           <h2>Navbar</h2> 
+         
            <nav>
+
+           <h2>Navbar</h2>
+           <NavLink to="/users">Users</NavLink> 
             <a href="/">Home</a>
             <a href="/about">About</a>
             <a href="/contact">Contact Us</a>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import { Outlet, useLocation, useNavgation} from "react-router-dom";
+import Footer from "../Footer/Footer";
 import Header from '../../assets/Header/Header';
 
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location);
     return (
         <div>
 
             <Header></Header>
-            <h2>This is the home componets</h2>
+
+            {
+                navigation.state === "loading" ?
+                <p>Loading...</p>:
+                    <h2>This is the home componets</h2>
+            <Outlet></Outlet>
+            }
+            <Footer></Footer>
         </div>
     );
-};
+ };
 
 export default Home;
